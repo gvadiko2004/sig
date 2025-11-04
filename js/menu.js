bf311329a57750be31f3a7257ad437e00bb47e38
gvadiko2004 <archilbigvava@gmail.com>
2025-11-04 19:55:41 +0300

    push

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,11 @@ const menu = document.querySelector(".menu-phone");
 
 btnMenu.addEventListener("click", function () {
   menu.classList.toggle("active");
+
+  // Блокировка / разблокировка скролла
+  if (menu.classList.contains("active")) {
+    document.body.style.overflow = "hidden"; // блокируем скролл
+  } else {
+    document.body.style.overflow = ""; // возвращаем (очищаем стиль)
+  }
 });
